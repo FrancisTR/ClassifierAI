229d7f63aad0e664f286a46cc744e3e2c44a164a
Francis Tran <xst-tran6832@stthomas.edu>
2026-05-29 14:32:39 -0500

    Translate to Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./public/manifest.json";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [crx({ manifest })],
+  plugins: [crx({ manifest }), tailwindcss()],
 });
